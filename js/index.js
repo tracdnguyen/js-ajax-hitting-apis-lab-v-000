2984dfcef87d9c0176ed04398638c18bd31dce9f
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-17 21:36:17 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // your code here
+function getRepositories(){
+  const req = new XMLHttpRequest();
+  const username = document.getElementById('username').value
+  req.addEventListener('load', listRepos);
+  req.open('GET', `https://api.github.com/users/${username}/repos`)
+  req.send();
+}
