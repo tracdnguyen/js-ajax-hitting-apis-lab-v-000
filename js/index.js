6621406da1c281b95a9bb6789b378d079fe9dd7f
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-17 22:06:56 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -10,9 +10,24 @@ function getRepositories(){
 function displayRepositories(){
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos.map(r =>
-  '<li>' + r.name + '- <a href="#" data-repo="' +
-  r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
-
+  const repoList = 
+    '<ul>' + 
+      repos.map(r => {
+        const dataUsername = 'data-username="' + r.owner.login + '"';
+        const dataRepo = 'data-repo="' + r.name + '"';
+        return `
+          <li> 
+            ${r.name}
+            <a href="$r.html_url">${r.html_url}</a>
+          
+          </li`
+      })
   document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el){
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', )
 }
