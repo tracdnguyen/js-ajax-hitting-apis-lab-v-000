f2631922c0e319e3e2d2c86260842831a9af12bc
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-17 22:36:16 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -59,5 +59,9 @@ function getBranches(el){
 }
 
 function displayBranches() {
-
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+    .map(branch => '<li>' + branch.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
 }
