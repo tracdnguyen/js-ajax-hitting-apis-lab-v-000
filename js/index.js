e9865d87767990c2b0d1618e002f460ff7b00440
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-17 21:45:44 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 // your code here
 function getRepositories(){
   const req = new XMLHttpRequest();
-  const username = document.getElementById('username').value
-  req.addEventListener('load', listRepos);
-  req.open('GET', `https://api.github.com/users/${username}/repos`)
+  const username = document.getElementById('username').value;
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
   req.send();
+}
+
+function displayRepositories(){
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos.map(r => 
+  '<li>' + r.name + '- <a href="#" data-repo="' +
+  r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
+
+  document.getElementById('repositories').innerHTML = repoList;
 }
